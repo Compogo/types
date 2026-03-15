bce4a731570b8d438a24937e27cb006abd6e2b0a
diez37 <diezinthead@gmail.com>
2026-03-15 03:53:39 +0300

    emitter add wait method

--- emitter/emitter.go
+++ emitter/emitter.go
@@ -24,14 +24,17 @@ type Emitter[T any] interface {
 	// Subscribe adds a new subscriber to the emitter.
 	// Returns an UnsubscribeFunc that can be used to remove the subscriber.
 	Subscribe(subscriber Subscriber[T]) UnsubscribeFunc
+
+	Wait()
 }
 
 // emitter is the internal implementation of Emitter interface.
 // It uses atomic counter for unique IDs and RWMutex for thread safety.
 type emitter[T any] struct {
 	subscribers map[uint64]Subscriber[T]
-	mutex       *sync.RWMutex
+	mutex       sync.RWMutex
 	nextID      atomic.Uint64
+	wg          sync.WaitGroup
 }
 
 // NewEmitter creates a new Emitter instance.
@@ -40,7 +43,6 @@ type emitter[T any] struct {
 func NewEmitter[T any]() Emitter[T] {
 	return &emitter[T]{
 		subscribers: make(map[uint64]Subscriber[T]),
-		mutex:       &sync.RWMutex{},
 	}
 }
 
@@ -51,10 +53,12 @@ func (emitter *emitter[T]) Emit(ctx context.Context, value T) {
 	emitter.mutex.RLock()
 	defer emitter.mutex.RUnlock()
 
+	emitter.wg.Add(len(emitter.subscribers))
 	for _, subscriber := range emitter.subscribers {
 		go func(ctx context.Context, subscriber Subscriber[T]) {
 			ctx, cancelFunc := context.WithCancel(ctx)
 			defer cancelFunc()
+			defer emitter.wg.Done()
 
 			subscriber(value)
 		}(ctx, subscriber)
@@ -84,4 +88,8 @@ func (emitter *emitter[T]) unsubscribe(id uint64) {
 	defer emitter.mutex.Unlock()
 
 	delete(emitter.subscribers, id)
+}
+
+func (emitter *emitter[T]) Wait() {
+	emitter.wg.Wait()
 }
